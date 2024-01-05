ed6b83ddcc981e462edbbee3c5ebedc94ec671b5
dtinth on MBP M1 <dtinth@spacet.me>
2024-01-06 02:41:48 +0700

    Refactor MonthlyTable into RankingColumn

--- docs/community/stats/MonthlyTable.vue
+++ docs/community/stats/MonthlyTable.vue
@@ -1,26 +1,15 @@
 <template>
   <div style="overflow-x: auto; overscroll-behavior: auto; display: flex; gap: 1rem">
-    <div style="flex: 0 0 300px; background: var(--vp-c-bg-soft); padding: 1rem; border-radius: 1rem;"
-      v-for="month of months" :key="month.key">
-      <strong>{{ month.name }} {{ month.year }}</strong>
-      <ol style="padding-left: 1.4em;">
-        <li v-for="user of month.users" style="padding-left: 0.2em;">
-          <span style="display: flex">
-            <strong style="flex: 1">{{ user.name }}</strong>
-            <span style="flex: none; opacity: 0.64;">{{ user.hoursSeen.toFixed(1) }}h</span>
-          </span>
-          <div style="height: 5px; overflow: hidden; background: #fff3; border-radius: 5px; display: flex;"
-            :style="{ width: user.percent.toFixed(1) + '%' }">
-            <div v-for="server of user.servers" :style="{ flex: server.hoursSeen }" :data-category="server.category"
-              class="server-bar"></div>
-          </div>
-        </li>
-      </ol>
-    </div>
+    <template v-for="month of months" :key="month.key">
+      <div style="flex: 0 0 256px; background: var(--vp-c-bg-soft); padding: 1rem; border-radius: 1rem;">
+        <RankingColumn :title="`${month.name} ${month.year}`" :users="month.users" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
+import RankingColumn from './RankingColumn.vue'
 const props = defineProps<{
   data: any
 }>()
@@ -44,20 +33,3 @@ const months = props.data.byYearMonth.map(({ year, month, byName }) => {
   }
 })
 </script>
-
-<style scoped>
-.server-bar {
-  background: var(--vp-c-yellow-3);
-}
-
-.server-bar[data-category="mjth"] {
-  background: var(--vp-c-indigo-3);
-}
-
-.server-bar[data-category="jamulusth"] {
-  background: var(--vp-c-green-3);
-}
-
-.server-bar[data-category="dharma"] {
-  background: var(--vp-c-red-3);
-}</style>
---
+++ docs/community/stats/RankingColumn.vue
@@ -0,0 +1,54 @@
+<template>
+  <strong>{{ props.title }}</strong>
+  <ol style="padding-left: 1.4em;" :start="props.start || 1">
+    <li v-for="user of props.users" style="padding-left: 0.2em;">
+      <span style="display: flex">
+        <strong style="flex: 1; white-space: nowrap;">{{ user.name }}</strong>
+        <span style="flex: none; opacity: 0.64;">{{ user.hoursSeen.toFixed(1) }}h</span>
+      </span>
+      <div style="height: 5px; overflow: hidden; background: #fff3; border-radius: 5px; display: flex;"
+        :style="{ width: user.percent.toFixed(1) + '%' }">
+        <div v-for="server of user.servers" :style="{ flex: server.hoursSeen }" :data-category="server.category"
+          class="server-bar"></div>
+      </div>
+    </li>
+  </ol>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  users: UserRow[]
+  start?: number
+}>()
+
+export interface UserRow {
+  name: string
+  hoursSeen: number
+  percent: number
+  servers: ServerStat[]
+}
+
+export interface ServerStat {
+  category: string
+  hoursSeen: number
+}
+</script>
+
+<style scoped>
+.server-bar {
+  background: var(--vp-c-yellow-3);
+}
+
+.server-bar[data-category="mjth"] {
+  background: var(--vp-c-indigo-3);
+}
+
+.server-bar[data-category="jamulusth"] {
+  background: var(--vp-c-green-3);
+}
+
+.server-bar[data-category="dharma"] {
+  background: var(--vp-c-red-3);
+}
+</style>
