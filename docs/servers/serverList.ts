fd3852d17ff160b78262af9a42b66c8eaed0df41
dtinth on MBP M1 <dtinth@spacet.me>
2024-02-10 14:45:03 +0700

    use new server list infra

--- docs/servers/serverList.ts
+++ docs/servers/serverList.ts
@@ -1,31 +1,23 @@
 import { atom, onMount } from "nanostores";
 import { getInstrumentImageUrl } from "./instruments";
 
-interface ServerList {
+interface Server {
   count: number;
-  maxTime: string;
-  servers: RawServer[];
-}
-interface RawServer {
-  count: number;
-  clients: ClientInfo[];
   listenUrl?: string;
   port: number;
   address: string;
   name: string;
   genre: string;
-}
-interface Server extends RawServer {
   musicians: Musician[];
   listeners: number;
   addressWithPort: string;
+
+  _clients?: JamulusExplorerClient[];
 }
-interface ClientInfo {
+interface Musician {
   name: string;
   instrument: string;
   skill: string;
-}
-export interface Musician extends ClientInfo {
   imageUrl?: string;
 }
 
@@ -33,52 +25,148 @@ export const $list = atom<Server[]>([]);
 export const $count = atom(0);
 export const $lastUpdated = atom("…");
 
+interface InformationFile {
+  key: string;
+}
+interface DataPoint {
+  time: string;
+  genre: string;
+  list: JamulusExplorerServer[];
+}
+interface JamulusExplorerServer {
+  numip: number;
+  port: number;
+  country: string;
+  maxclients: number;
+  perm: number;
+  name: string;
+  ipaddrs: string;
+  city: string;
+  ip: string;
+  ping: number;
+  os: string;
+  version: string;
+  versionsort: string;
+  nclients?: number;
+  clients?: JamulusExplorerClient[];
+  index: number;
+}
+export interface JamulusExplorerClient {
+  chanid: number;
+  country: string;
+  instrument: string;
+  skill: string;
+  name: string;
+  city: string;
+}
+
+const listenUrls = {
+  "150.95.25.226": "https://lobby.musicjammingth.net/",
+  "103.246.19.200": "https://lobby.jamulusth.com/",
+  "103.91.189.71": "https://lobbypro.jamulusth.com/",
+};
+
 async function refresh() {
-  const data = await fetch(
-    "https://json-data-storage-default-rtdb.asia-southeast1.firebasedatabase.app/tenants/thailand-jamulus-user-group/data/servers.json"
-  )
-    .then((r) => r.json())
-    .then((t) => JSON.parse(t.json) as ServerList);
-  $count.set(data.count);
-  $lastUpdated.set(
-    new Date(data.maxTime).toLocaleString("th-TH", {
-      timeZone: "Asia/Bangkok",
-    })
+  const info = await fetch(
+    "https://jamulus-archive.ap-south-1.linodeobjects.com/main/latest.json"
+  ).then((r) => r.json() as Promise<InformationFile>);
+
+  const datapoints = await fetch(
+    `https://jamulus-archive.ap-south-1.linodeobjects.com/${info.key}`
+  ).then((r) => r.json() as Promise<DataPoint[]>);
+
+  const serversInThailand = datapoints.flatMap((d) =>
+    d.list
+      .filter((s) => s.country === "Thailand" || s.city === "Uranus")
+      .map((s) => ({
+        ...s,
+        genre: d.genre,
+      }))
   );
-  const list: Server[] = data.servers
-    .sort((a, b) => b.count - a.count)
-    .map((s) => {
-      const clients = s.clients.map((c): Musician => {
+  const count = serversInThailand.length;
+
+  const maxTime = datapoints
+    .map((d) => new Date(d.time).getTime())
+    .reduce((a, b) => Math.max(a, b), 0);
+  const lastUpdated = new Date(maxTime).toLocaleString("th-TH", {
+    timeZone: "Asia/Bangkok",
+  });
+
+  const list: Server[] = serversInThailand
+    .map((s): Server => {
+      const musicians: Musician[] = [];
+      let listeners = 0;
+      const listenUrl = listenUrls[s.ip] ?? undefined;
+      for (const c of s.clients ?? []) {
         const instrument = c.instrument;
+        if (instrument === "Streamer" || instrument === "Recorder") {
+          const m = c.name.match(/^[ ][ ]lobby \[(\d+)\][ ][ ]$/);
+          if (m) {
+            listeners += parseInt(m[1]);
+          }
+          continue;
+        }
         const imageUrl = getInstrumentImageUrl(instrument);
-        return {
+        musicians.push({
           ...c,
           imageUrl: imageUrl,
-        };
-      });
-      const listeners = clients.reduce((a, c) => {
-        const match = c.name.match(/^[ ][ ]lobby \[(\d+)\][ ][ ]$/);
-        if (!match) return a;
-        if (!s.listenUrl) return a;
-        if (c.skill !== "Expert") return a;
-        if (c.instrument !== "Streamer") return a;
-        return a + parseInt(match[1]);
-      }, 0);
+        });
+      }
+
       return {
-        ...s,
-        musicians: clients.filter((c) => c.instrument !== "Streamer"),
+        count: musicians.length,
+        musicians,
+        port: s.port,
+        address: s.ip,
+        name: s.name,
+        genre: s.genre,
         listeners,
-        // musicians: clients,
-        addressWithPort: s.port == 22124 ? s.address : `${s.address}:${s.port}`,
-      } as Server;
-    });
+        addressWithPort: s.port == 22124 ? s.ip : `${s.ip}:${s.port}`,
+        listenUrl,
+        _clients: s.clients,
+      };
+    })
+    .sort((a, b) => b.count - a.count);
+
+  $count.set(count);
+  $lastUpdated.set(lastUpdated);
   $list.set(list);
-  // if (!navigator.userAgent.includes("Googlebot")) {
-  //     document.title = this.count
-  //         ? `(${this.count}) ${originalTitle}`
-  //         : originalTitle;
-  // }
-  console.log({ data, list });
+  console.log({ list });
+
+  // const list: Server[] = data.servers
+  //   .sort((a, b) => b.count - a.count)
+  //   .map((s) => {
+  //     const clients = s.clients.map((c): Musician => {
+  //       const instrument = c.instrument;
+  //       const imageUrl = getInstrumentImageUrl(instrument);
+  //       return {
+  //         ...c,
+  //         imageUrl: imageUrl,
+  //       };
+  //     });
+  //     const listeners = clients.reduce((a, c) => {
+  //       const match = c.name.match(/^[ ][ ]lobby \[(\d+)\][ ][ ]$/);
+  //       if (!match) return a;
+  //       if (!s.listenUrl) return a;
+  //       if (c.skill !== "Expert") return a;
+  //       if (c.instrument !== "Streamer") return a;
+  //       return a + parseInt(match[1]);
+  //     }, 0);
+  //     return {
+  //       ...s,
+  //       musicians: clients.filter((c) => c.instrument !== "Streamer"),
+  //       listeners,
+  //       // musicians: clients,
+  //       addressWithPort: s.port == 22124 ? s.address : `${s.address}:${s.port}`,
+  //     } as Server;
+  //   });
+  // $list.set(list);
+  // // if (!navigator.userAgent.includes("Googlebot")) {
+  // //     document.title = this.count
+  // //         ? `(${this.count}) ${originalTitle}`
+  // //         : originalTitle;
+  // // }
+  // console.log({ data, list });
 }
 
 if (typeof window !== "undefined") {
